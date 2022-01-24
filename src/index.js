07c87ec635a2798af4a8d69b299139f48f6e4576
Evkolkovskiy <evgeny.kolkovski@gmail.com>
2022-01-24 18:16:31 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprWhithoutSpaces = expr.split('**********'); //выводим массив разбитый на элементы между закодированными пробелами (т.е получили массив из закодированных слов)
+    let exprToWords = exprWhithoutSpaces.map(function(codeOfWord) {
+        let word = '';
+        for (let i = 0; i < codeOfWord.length; i = i + 10) {
+            let numberToSign = '';
+            let letter = codeOfWord.slice(i, i+10);                         //берем букву, т.е 10 символов
+            for (let j = letter.length-1; j >= 0; j = j - 2) {              //пробегаемся по коду буквы и удаляем нули, меняем 10 и 11 на символы
+                if ((letter[j-1] + letter[j]) === '10') {
+                    numberToSign = '.' + numberToSign;
+                }
+                if (letter[j-1] + letter[j] === '11') {
+                    numberToSign = '-' + numberToSign;
+                }
+            }
+            word += MORSE_TABLE[numberToSign];                              //составляем слово из ьаблицы
+        }
+        return word;
+    });
+    let result = exprToWords.join(' ');                                     //объдиняем элементы из массива в строку
+    return result;
+
 }
 
 module.exports = {
